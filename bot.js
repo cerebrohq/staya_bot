49464c06fb7124d2d38810e20370afbd95cbe097
Вербитский Артем <ehtemka@cerebrohq.com>
2017-09-11 17:53:52 +0300

    debug key

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ var query = require('./func');
 var trace = require('./trace');
 
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 5c9489ee-c5bc-4bc9-8f8b-879fd2998f71,//process.env.MICROSOFT_APP_ID,
+    appPassword: 9ogUpVe5sP5x084Eu0wcgdF//process.env.MICROSOFT_APP_PASSWORD
 });
 
 
